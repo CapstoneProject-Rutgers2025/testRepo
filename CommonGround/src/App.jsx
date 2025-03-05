7e3121cbedb9ecd5dfd968c9298685649d710573
YusraaElshamma <yusrakhaled@yahoo.com>
2025-03-05 14:02:57 -0500

    Update App.jsx

--- CommonGround/src/App.jsx
+++ CommonGround/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './login';  
 import SignUp from './signup'; 
-import Dashboard from "./Dashboard";
+import Dashboard from "./dashboard";
 
 
 function App() {
