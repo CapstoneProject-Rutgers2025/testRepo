126fc35941e1848bb0fa285030398e69fdda85c3
YusraaElshamma <yusrakhaled@yahoo.com>
2025-03-11 00:18:28 -0400

    updates too frontend

--- CommonGround/src/App.jsx
+++ CommonGround/src/App.jsx
@@ -3,15 +3,18 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './login';  
 import SignUp from './signup'; 
 import Dashboard from "./dashboard";
+import HomeScreen from './homescreen';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />  
+        <Route path="/" element={<HomeScreen />} /> 
+        <Route path="/login" element={<Login />} /> 
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
+
       </Routes>
     </Router>
   );
