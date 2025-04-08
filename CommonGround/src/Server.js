3e0ec29484e72951c8c70a85b7c9362633051b06
Eric Hernandez <141759184+erichernandez1703@users.noreply.github.com>
2025-04-08 15:28:26 -0400

    Update Server.js

--- CommonGround/src/Server.js
+++ CommonGround/src/Server.js
@@ -35,7 +35,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ['https://commonground.netlify.app'],
+  origin: ['https://commonnground.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
