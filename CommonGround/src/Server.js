49a56f54a12d6a9fe715ea558b0c2014602ddfdb
Abdalrhman saed <boodysaedt2@gmail.com>
2025-04-11 16:01:16 -0400

    bug fix #2

--- CommonGround/src/Server.js
+++ CommonGround/src/Server.js
@@ -328,8 +328,6 @@ app.post('/posts', upload.single('image'), async (req, res) => {
     }
 
 
-// Start the server
-const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
