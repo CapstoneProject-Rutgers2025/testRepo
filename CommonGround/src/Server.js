eccd68a392f90ca22f9c064fc230641699c23dc8
Abdalrhman saed <boodysaedt2@gmail.com>
2025-04-18 14:55:13 -0400

    Testijng Websocket fix

--- CommonGround/src/Server.js
+++ CommonGround/src/Server.js
@@ -50,6 +50,7 @@ const io = new Server(server, {
     methods: ['GET', 'POST'],
     credentials: true,
   },
+  transports: ['websocket'], 
 });
 
 // Attach `io` to the app for global access
--- CommonGround/src/socket.js
+++ CommonGround/src/socket.js
@@ -2,8 +2,8 @@ import { io } from 'socket.io-client';
 
 const BASE_URL =
   import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_RENDER_URL || "https://testrepo-hkzu.onrender.com"
-    : import.meta.env.VITE_LOCAL_URL || "http://localhost:3000";
+    ? "https://testrepo-hkzu.onrender.com"
+    : "http://localhost:3000";
 
 const socket = io(BASE_URL, {
   transports: ['websocket'], // Force WebSocket transport
