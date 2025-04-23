397bcf5f62d4786c96d8e6901af94a84734ff89b
Abdalrhman saed <boodysaedt2@gmail.com>
2025-04-23 14:13:49 -0400

    Atempting to fix error again

--- CommonGround/src/sidebar/chat.jsx
+++ CommonGround/src/sidebar/chat.jsx
@@ -4,10 +4,7 @@ import { jwtDecode } from 'jwt-decode';
 import './chat.css';
 import ChatBubble from './chatbubble';
 
-const BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.VITE_RENDER_URL
-    : process.env.VITE_LOCAL_URL;
+const BASE_URL = 'https://testrepo-hkzu.onrender.com';
 
 const socket = io(BASE_URL);
 
